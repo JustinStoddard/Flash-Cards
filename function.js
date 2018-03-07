aa8cdbfef06d6e2dbfa50a328f2d476ef87ef762
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-03-06 17:31:43 -0700

    New Updates

--- function.js
+++ function.js
@@ -1,19 +1,36 @@
-let showForm = false
+let titles = []
+let descriptions = []
+let index = 0;
 
-function toggleForm() {
-  showForm = !showForm
-  $('#form1A').toggle();
-}
+const CreateCard = (title, description) => {
+  i++;
+  titles[index] = title;
+  answers[index] = description;
+  card = `${title}`
+  $('#flashcards').append(`
+  <div data-id="${index}">
+    <div class="row">
+      <div class="col s12 m3">
+        <div class="card blue-grey darken-1">
+          <div class="card-content">
+            <span class="card-title">${card}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <a href="#" data-d="${index}" id="show-answer">Show Answer</a>
+    </div>
+  </div>
+  `)
+};
 
-$(document).ready(function(){
-  $('#toggle1').on('click', function() {
-    toggleForm()
+$(document).ready( function() {
+  $(document).on('submit', '#form1A', function(foo) {
+    foo.preventDefault();
+    CreateCard((document.getElementById('title').value),(document.getElementById('description').value))
   });
 });
-
-$(document).on('submit', '#form1A', function() {
-  
-})
 
 
 
